a2a6169a977b30f930491e60d87c019d139fd6c6
huaz <1950796924@qq.com>
2024-07-14 23:13:11 +0800

    feat: import commonjs

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import { type PluginOption } from 'vite';
+import { type PluginOption } from "vite";
 
-import httpProxy, { createProxyServer } from 'http-proxy';
-import { IncomingMessage, ServerResponse } from 'http';
-
-const PLUGIN_NAME = 'vite-plugin-api-proxy';
-const proxy:httpProxy = createProxyServer();
+import httpProxy from "http-proxy";
+import { IncomingMessage, ServerResponse } from "http";
+const { createProxyServer } = httpProxy;
+const PLUGIN_NAME = "vite-plugin-api-proxy";
+const proxy: httpProxy = createProxyServer();
 
 export interface apiMockOption {
-  mockServerMap: Map<string|RegExp, string>;
+  mockServerMap: Map<string | RegExp, string>;
   printLog: boolean;
 }
-async function requestMiddleware( opt: apiMockOption) {
+async function requestMiddleware(opt: apiMockOption) {
   const { printLog = true, mockServerMap } = opt;
   const mockItem = Array.from(mockServerMap);
   mockItem.forEach((item) => {
@@ -25,21 +25,21 @@ async function requestMiddleware( opt: apiMockOption) {
     next: () => void
   ) => {
     const matchRequest = mockItem.find(([prefix, target]) => {
-      if(!(prefix instanceof RegExp)){
-        prefix = new RegExp(`^${prefix}`)
+      if (!(prefix instanceof RegExp)) {
+        prefix = new RegExp(`^${prefix}`);
       }
       return prefix.test(req.url);
     });
 
     if (matchRequest) {
       const prevUrl = matchRequest[0];
-      const proxyUrl = req.url?.replace(new RegExp(`^${prevUrl}`), '');
+      const proxyUrl = req.url?.replace(new RegExp(`^${prevUrl}`), "");
       printLog &&
         console.log(`mock:[${req.url}] => `, matchRequest[1] + proxyUrl);
       req.url = proxyUrl;
       proxy.web(req, res, {
         target: matchRequest[1],
-        changeOrigin: true // 为跨域请求修改origin
+        changeOrigin: true, // 为跨域请求修改origin
       });
 
       return;
@@ -52,17 +52,17 @@ async function requestMiddleware( opt: apiMockOption) {
 function devProxy(opt: apiMockOption): PluginOption {
   return {
     name: PLUGIN_NAME,
-    apply: 'serve',
-    enforce: 'pre',
+    apply: "serve",
+    enforce: "pre",
     configureServer: async ({ middlewares }) => {
       const { mockServerMap } = opt;
       if (mockServerMap.size < 1) {
         return;
       }
 
-      const middleware = await requestMiddleware( opt);
+      const middleware = await requestMiddleware(opt);
       middlewares.use(middleware);
-    }
+    },
   };
 }
 
