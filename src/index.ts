81cef6a437dd4af597b387df8d8bf28bc4ce3ea2
郑华 <1950796924@qq.com>
2024-07-15 10:44:13 +0800

    feat: 代理参数不在使用Map

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,19 @@ const PLUGIN_NAME = "vite-plugin-api-proxy";
 const proxy: httpProxy = createProxyServer();
 
 export interface apiMockOption {
-  mockServerMap: Map<string | RegExp, string>;
+  mockServerMap: [string | RegExp, string][];
   printLog: boolean;
 }
 async function requestMiddleware(opt: apiMockOption) {
   const { printLog = true, mockServerMap } = opt;
-  const mockItem = Array.from(mockServerMap);
-  mockItem.forEach((item) => {
+   
+  mockServerMap.forEach((item) => {
     console.log(
       `已为您将${item[0]}前缀请求代理到mock服务器:${item[1]},请确保服务器能正常访问`
     );
+    if (!(item[0] instanceof RegExp)) {
+      item[0] = new RegExp(`^${item[0]}`);
+    }
   });
 
   const middleware = async (
@@ -24,16 +27,14 @@ async function requestMiddleware(opt: apiMockOption) {
     res: ServerResponse,
     next: () => void
   ) => {
-    const matchRequest = mockItem.find(([prefix, target]) => {
-      if (!(prefix instanceof RegExp)) {
-        prefix = new RegExp(`^${prefix}`);
-      }
-      return prefix.test(req.url);
+    const matchRequest = mockServerMap.find(([prefix , target]) => {
+      return (prefix as RegExp).test(req.url);
     });
 
     if (matchRequest) {
       const prevUrl = matchRequest[0];
-      const proxyUrl = req.url?.replace(new RegExp(`^${prevUrl}`), "");
+      const proxyUrl = req.url?.replace(prevUrl, '');
+
       printLog &&
         console.log(`mock:[${req.url}] => `, matchRequest[1] + proxyUrl);
       req.url = proxyUrl;
@@ -56,7 +57,7 @@ function devProxy(opt: apiMockOption): PluginOption {
     enforce: "pre",
     configureServer: async ({ middlewares }) => {
       const { mockServerMap } = opt;
-      if (mockServerMap.size < 1) {
+      if (mockServerMap.length < 1) {
         return;
       }
 
